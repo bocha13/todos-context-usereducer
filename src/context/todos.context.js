1e5d58f235b890df58201dd0df94b30bb7c49adc
bocha13 <ignaciobochatay@gmail.com>
2019-10-01 16:02:17 -0300

    added consumed context by hooks

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -5,15 +5,15 @@ const defaultTodos = [
     { id: 1, task: "Mow the lawn using goats", completed: false},
     { id: 2, task: "Release lady bugs into garden", completed: true}
 ]
-export const todosContext = createContext();
+export const TodosContext = createContext();
 
 export const TodosProvider = props => {
-    const {todos, addTodo, removeTodo, toggleTodo, editTodo} = useTodoState(defaultTodos);
+    const todosStuff = useTodoState(defaultTodos);
 
     return (
-        <todosContext.Provider value={{ todos, addTodo, removeTodo, toggleTodo, editTodo}}>
+        <TodosContext.Provider value={todosStuff}>
             {props.children}
-        </todosContext.Provider>
+        </TodosContext.Provider>
     )
 }
 
